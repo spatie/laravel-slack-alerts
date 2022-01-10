bb4e90da1ccf5ea8057230bcc7462432284e16a2
freek <freek@spatie.be>
2022-01-10 08:48:45 +0100

    simplify testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,29 +8,10 @@ use Spatie\SlackLogger\SlackLoggerServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Spatie\\SlackLogger\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app): array
     {
         return [
             SlackLoggerServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-slack-logger_table.php.stub';
-        $migration->up();
-        */
     }
 }
