eb7db6d61fa99bce8bafcf8ce94e0f92c85aef5f
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2022-01-11 15:37:50 +0100

    add typehinting on Facade

--- src/Facades/SlackAlert.php
+++ src/Facades/SlackAlert.php
@@ -5,6 +5,9 @@ namespace Spatie\SlackAlerts\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static self to(string $text)
+ * @method static void message(string $text)
+ *
  * @see \Spatie\SlackAlerts\SlackAlert
  */
 class SlackAlert extends Facade
