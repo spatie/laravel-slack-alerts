77c46b965750a3fab654af9d4aa5c46c5982e530
Bogdan Lopatkin <bogdan.lopatkin@gmail.com>
2023-06-26 02:15:34 +0200

    Feature: Added ability to send message to a custom channel.

--- src/Facades/SlackAlert.php
+++ src/Facades/SlackAlert.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static self to(string $text)
+ * @method static self toChannel(string $text)
  * @method static void message(string $text)
  * @method static void blocks(array $blocks)
  *
